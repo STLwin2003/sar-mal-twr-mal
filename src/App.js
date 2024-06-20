bc1972f04fe0f63606048719c936dd8c5f2ccda2
STLwin2003 <shinthantlwin70@gmail.com>
2024-06-20 13:47:41 +0630

    components file is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import StreetFood from "./components/StreetFood";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <StreetFood />
+    </div>
+  );
 };
 
 export default App;
