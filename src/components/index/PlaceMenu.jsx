55d246e4f319e2bdb38ed31f8f3591fbe9d4fe0f
YadanarHtunMyint <htunmyinty@gmail.com>
2024-06-25 14:44:58 +0930

    place menu is added

--- src/components/index/PlaceMenu.jsx
+++ src/components/index/PlaceMenu.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const PlaceMenu = () => {
-  return <div>Please write code here between the div</div>;
+  return <div>
+    <div className="container mt-5">
+    <h1><p className="brown">PLACE LIST</p></h1>
+  <div className="row">
+    <div className="col-sm-6 col-md-4 col-lg-3">
+    <img src="https://media.istockphoto.com/id/1134159071/photo/empty-cafeteria.jpg?s=1024x1024&w=is&k=20&c=FBg6g4CvuWzbAWi_fgaPxPUTXsmk_dQJdhe4_8aqNbk=" className="rounded mb-3" alt="Cinque Terre" width="280" height="300"/> 
+      <h4><p className="brown"><b>Pathein Park</b></p></h4>
+      
+    </div>
+    <div className="col-sm-6 col-md-4 col-lg-3">
+    <img src="https://media.istockphoto.com/id/1134159071/photo/empty-cafeteria.jpg?s=1024x1024&w=is&k=20&c=FBg6g4CvuWzbAWi_fgaPxPUTXsmk_dQJdhe4_8aqNbk=" className="rounded mb-3" alt="Cinque Terre" width="280" height="300"/> 
+    <h4><p className="brown"><b>Pathein Park</b></p></h4>
+      
+    </div>
+    <div className="col-sm-6 col-md-4 col-lg-3">
+    <img src="https://media.istockphoto.com/id/1134159071/photo/empty-cafeteria.jpg?s=1024x1024&w=is&k=20&c=FBg6g4CvuWzbAWi_fgaPxPUTXsmk_dQJdhe4_8aqNbk=" className="rounded mb-3" alt="Cinque Terre" width="280" height="300"/> 
+    <h4><p className="brown"><b>Pathein Park</b></p></h4>        
+     
+    </div>
+    <div className="col-sm-6 col-md-4 col-lg-3">
+    <img src="https://media.istockphoto.com/id/1134159071/photo/empty-cafeteria.jpg?s=1024x1024&w=is&k=20&c=FBg6g4CvuWzbAWi_fgaPxPUTXsmk_dQJdhe4_8aqNbk=" className="rounded mb-3" alt="Cinque Terre" width="280" height="300"/> 
+    <h4><p className="brown"><b>Pathein Park</b></p></h4>        
+      
+    </div>
+  </div>
+  <div>
+<button type="button" class="btn-brown float-end">see more</button>
+</div>
+</div> 
+
+  </div>;
 };
 
 export default PlaceMenu;
