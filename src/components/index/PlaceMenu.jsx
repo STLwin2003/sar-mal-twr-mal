0fb6b04f5d373900affcbf323d9023626d705bd2
YadanarHtunMyint <htunmyinty@gmail.com>
2024-06-26 17:21:04 +0930

    {change}font size

--- src/components/index/PlaceMenu.jsx
+++ src/components/index/PlaceMenu.jsx
@@ -3,31 +3,33 @@ import React from "react";
 const PlaceMenu = () => {
   return <div>
     <div className="container mt-5">
-    <h1><p className="brown">PLACE LIST</p></h1>
+    <h3><p className="brown">PLACE LIST</p></h3>
   <div className="row">
     <div className="col-sm-6 col-md-4 col-lg-3">
-    <img src="https://media.istockphoto.com/id/1134159071/photo/empty-cafeteria.jpg?s=1024x1024&w=is&k=20&c=FBg6g4CvuWzbAWi_fgaPxPUTXsmk_dQJdhe4_8aqNbk=" className="rounded mb-3" alt="Cinque Terre" width="280" height="300"/> 
-      <h4><p className="brown"><b>Pathein Park</b></p></h4>
+      
+    <img src="https://media.istockphoto.com/id/1134159071/photo/empty-cafeteria.jpg?s=1024x1024&w=is&k=20&c=FBg6g4CvuWzbAWi_fgaPxPUTXsmk_dQJdhe4_8aqNbk=" className="rounded mb-3" alt="Cinque Terre" width="300" height="300" row-gap="10px" column-gap="15px"/> 
+      <p className="fs-5"><span class="brown">Pathein Park</span></p>
+      
+    </div>
+      
+    <div className="col-sm-6 col-md-4 col-lg-3">
+    <img src="https://media.istockphoto.com/id/1134159071/photo/empty-cafeteria.jpg?s=1024x1024&w=is&k=20&c=FBg6g4CvuWzbAWi_fgaPxPUTXsmk_dQJdhe4_8aqNbk=" className="rounded mb-3" alt="Cinque Terre" width="300" height="300" row-gap="10px" column-gap="15px"/> 
+    <p className="fs-5"><span class="brown">Pathein Park</span></p>
       
     </div>
     <div className="col-sm-6 col-md-4 col-lg-3">
-    <img src="https://media.istockphoto.com/id/1134159071/photo/empty-cafeteria.jpg?s=1024x1024&w=is&k=20&c=FBg6g4CvuWzbAWi_fgaPxPUTXsmk_dQJdhe4_8aqNbk=" className="rounded mb-3" alt="Cinque Terre" width="280" height="300"/> 
-    <h4><p className="brown"><b>Pathein Park</b></p></h4>
-      
-    </div>
-    <div className="col-sm-6 col-md-4 col-lg-3">
-    <img src="https://media.istockphoto.com/id/1134159071/photo/empty-cafeteria.jpg?s=1024x1024&w=is&k=20&c=FBg6g4CvuWzbAWi_fgaPxPUTXsmk_dQJdhe4_8aqNbk=" className="rounded mb-3" alt="Cinque Terre" width="280" height="300"/> 
-    <h4><p className="brown"><b>Pathein Park</b></p></h4>        
+    <img src="https://media.istockphoto.com/id/1134159071/photo/empty-cafeteria.jpg?s=1024x1024&w=is&k=20&c=FBg6g4CvuWzbAWi_fgaPxPUTXsmk_dQJdhe4_8aqNbk=" className="rounded mb-3" alt="Cinque Terre" width="300" height="300" row-gap="10px" column-gap="15px"/> 
+    <p className="fs-5"><span class="brown">Pathein Park</span></p>
      
     </div>
     <div className="col-sm-6 col-md-4 col-lg-3">
-    <img src="https://media.istockphoto.com/id/1134159071/photo/empty-cafeteria.jpg?s=1024x1024&w=is&k=20&c=FBg6g4CvuWzbAWi_fgaPxPUTXsmk_dQJdhe4_8aqNbk=" className="rounded mb-3" alt="Cinque Terre" width="280" height="300"/> 
-    <h4><p className="brown"><b>Pathein Park</b></p></h4>        
+    <img src="https://media.istockphoto.com/id/1134159071/photo/empty-cafeteria.jpg?s=1024x1024&w=is&k=20&c=FBg6g4CvuWzbAWi_fgaPxPUTXsmk_dQJdhe4_8aqNbk=" className="rounded mb-3" alt="Cinque Terre" width="300" height="300" row-gap="10px" column-gap="15px"/> 
+    <p className="fs-5"><span class="brown">Pathein Park</span></p>     
       
     </div>
   </div>
-  <div>
-<button type="button" class="btn-brown float-end">see more</button>
+  <div className="pb-5">
+<button type="button" class="btn-brown float-end">See More</button>
 </div>
 </div> 
 
