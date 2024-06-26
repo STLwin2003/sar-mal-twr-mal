442892cfe56bd0b8db10c1e9d26ba02f3358522e
STLwin2003 <shinthantlwin70@gmail.com>
2024-06-26 19:19:42 +0630

    [update]main title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,66 +7,84 @@ const Header = () => {
     <div className="mb-5">
       <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top py-3 ">
         <div className="container">
-          <NavLink to="/" className="navbar-brand  fs-2 " href="#">
-            <span className="brown">SarMalTwrMal</span>
+          <NavLink to="/" className="navbar-brand  fs-1 " href="#">
+            <span className="brown lobster-regular">SarMalTwrMal</span>
           </NavLink>
+
+          <div
+            className="offcanvas offcanvas-end  "
+            id="offcanvasRight"
+            tabindex="-1"
+            aria-labelledby="offcanvasRightLabel"
+          >
+            <div className="offcanvas-header">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink
+                    to="/home"
+                    className="nav-link active me-3 "
+                    aria-current="page"
+                  >
+                    <span className="brown">Home</span>
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/street_food_list" className="nav-link me-3 ">
+                    <span className="brown">Street Food</span>
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/coffee_shop_list" className="nav-link me-3 ">
+                    <span className="brown">Coffee Shop</span>
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/place_list" className="nav-link me-3 ">
+                    <span className="brown">Place</span>
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <form className="d-flex ">
+            <div
+              className="my-auto fs-3 me-4 "
+              data-bs-toggle="offcanvas"
+              data-bs-target="#searchBar"
+            >
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </div>
+
+            {/* offcanvas start */}
+
+            {/* offcanvas end */}
+
+            <Link to="/detail">
+              <button className=" btn-brown rounded-3" type="submit">
+                <span>Login/Signup</span>
+              </button>
+            </Link>
+          </form>
           <button
             className="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasRight"
+            aria-controls="offcanvasRightLabel"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink
-                  to="/home"
-                  className="nav-link active me-3 "
-                  aria-current="page"
-                >
-                  <span className="brown">Home</span>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/street_food_list" className="nav-link me-3 ">
-                  <span className="brown">Street Food</span>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/coffee_shop_list" className="nav-link me-3 ">
-                  <span className="brown">Coffee Shop</span>
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/place_list" className="nav-link me-3 ">
-                  <span className="brown">Place</span>
-                </NavLink>
-              </li>
-              {/* <li className="nav-item">
-                <NavLink to="" className="nav-link me-2 ">
-                  <span className="brown">Contact</span>
-                </NavLink>
-              </li> */}
-            </ul>
-            <form className="d-flex">
-              <input
-                className="form-control me-2 fw-semibold"
-                type="search"
-                placeholder="Search . . . "
-                aria-label="Search"
-              />
-              <Link to="/detail">
-                <button className=" btn-brown rounded-3" type="submit">
-                  <span>Login</span>
-                </button>
-              </Link>
-            </form>
-          </div>
         </div>
       </nav>
       <div className="row my-5">
