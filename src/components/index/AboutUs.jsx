be956176bf75d5251b965d1d336d6a14a2ac2bb8
eithazinphyoe <eithazinphyoee@gmail.com>
2024-06-28 22:42:49 +0630

    The test

--- src/components/index/AboutUs.jsx
+++ src/components/index/AboutUs.jsx
@@ -2,20 +2,18 @@ import React from "react";
 
 const AboutUs = () => {
   return (<div>
-
-
-    <div className="container my-3 pt-5">
+    <div className="card container my-3 pt-5 bg-info p-5">
+      
       <div className="row mb-3">
-        <div className="col-sm container-fluid">
-          <div className="mt-5 text-center">
-            <p className="fs-1 fw-bold brown">About Us</p>
-          
-          <p className="fs-4 brown">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, quibusdam, libero dignissimos totam molestiae ea velit voluptates doloremque vitae tenetur magnam quos. Libero, recusandae cupiditate accusamus non a odit ratione?</p>
+        <div className="col-sm-7 container-fluid">
+          <div className=" text-right my-3 text-center mt-5">
+          <p className="fs-3 fw-semibold dark">About Us</p>
+          <p className="fs-4 medium dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, quibusdam, libero dignissimos totam molestiae ea velit voluptates doloremque vitae tenetur magnam quos. Libero, recusandae cupiditate accusamus non a odit ratione?</p>
         </div>
         </div>
-        <div className="col-sm container-fluid">
-          <div >
-            <img src="https://images.unsplash.com/photo-1551504734-5ee1c4a1479b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-100 h-100 " alt="" />
+        <div className="col-sm-5 container-fluid float-end ">
+          <div className="rounded-2">
+            <img src="https://st2.depositphotos.com/30291372/46142/v/450/depositphotos_461424770-stock-illustration-flat-vector-illustration-for-about.jpg" className="h-100 w-100 " alt="" />
           </div>
         </div>
       </div>
