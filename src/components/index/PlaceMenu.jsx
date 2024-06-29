398cf98e7280acf6385152c8c5a9d8250186050f
STLwin2003 <shinthantlwin70@gmail.com>
2024-06-29 09:32:47 +0630

    [update]place menu

--- src/components/index/PlaceMenu.jsx
+++ src/components/index/PlaceMenu.jsx
@@ -1,39 +1,37 @@
 import React from "react";
+import PlaceCard from "../card/PlaceCard";
 
 const PlaceMenu = () => {
-  return <div>
-    <div className="container mt-5">
-    <p className="brown fs-3 fw-semibold">PLACE LIST</p>
-  <div className="row">
-    <div className="col-sm-6 col-md-4 col-lg-3">
-      
-    <img src="https://jayscotts.com/wp-content/uploads/2022/03/restaurant-patio-design-6-821x1024.jpg" className="rounded mb-3 card-img-top" alt="Cinque Terre"/> 
-      <p className="fs-5 fw-medium"><span class="brown">Pathein Park</span></p>
-      
-    </div>
-      
-    <div className="col-sm-6 col-md-4 col-lg-3">
-    <img src="https://jayscotts.com/wp-content/uploads/2022/03/restaurant-patio-design-6-821x1024.jpg" className="rounded mb-3 card-img-top" alt="Cinque Terre"/> 
-    <p className="fs-5 fw-medium"><span class="brown">Pathein Park</span></p>
-      
-    </div>
-    <div className="col-sm-6 col-md-4 col-lg-3">
-    <img src="https://jayscotts.com/wp-content/uploads/2022/03/restaurant-patio-design-6-821x1024.jpg" className="rounded mb-3 card-img-top" alt="Cinque Terre"/> 
-    <p className="fs-5 fw-medium"><span class="brown">Pathein Park</span></p>
-     
-    </div>
-    <div className="col-sm-6 col-md-4 col-lg-3">
-    <img src="https://jayscotts.com/wp-content/uploads/2022/03/restaurant-patio-design-6-821x1024.jpg" className="rounded mb-3 card-img-top" alt="Cinque Terre"/> 
-    <p className="fs-5 fw-medium"><span class="brown">Pathein Park</span></p>     
-      
-    </div>
-  </div>
-  <div className="d-grid gap-2 d-md-flex justify-content-md-end " >
-           <button className="btn" type="button" style={{background:"#b46c14",color:"white"}}>See More</button>
+  return (
+    <div>
+      <div className="container mt-5">
+        <p className="brown fs-3 fw-semibold">PLACE LIST</p>
+        <div className="row">
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
         </div>
-</div> 
-
-  </div>;
+        <div className="d-grid gap-2 d-md-flex justify-content-md-end ">
+          <button
+            className="btn"
+            type="button"
+            style={{ background: "#b46c14", color: "white" }}
+          >
+            See More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlaceMenu;
