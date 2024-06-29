2c244c44bb4d1c8f8c1afa50ab33fa6b8ec38f7a
STLwin2003 <shinthantlwin70@gmail.com>
2024-06-29 10:22:23 +0630

    [update]place card and list

--- src/components/card/PlaceCard.jsx
+++ src/components/card/PlaceCard.jsx
@@ -7,10 +7,10 @@ const PlaceCard = () => {
         <img
           src="https://jayscotts.com/wp-content/uploads/2022/03/restaurant-patio-design-6-821x1024.jpg"
           className="rounded mb-3 card-img-top shadow-lg"
-          alt="Cinque Terre"
+          alt=""
         />
-        <p className="fs-5 mb-4 fw-medium">
-          <span className="">Pathein Park</span>
+        <p className="fs-5 mb-5 fw-medium">
+          <span>Pathein Park</span>
         </p>
       </div>
     </div>
--- src/components/list/PlaceList.jsx
+++ src/components/list/PlaceList.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import PlaceCard from "../card/PlaceCard";
 
 const PlaceList = () => {
-  return <div>PlaceList</div>;
+  return (
+    <div>
+      <div className="container my-5">
+        <p className="fs-2 text-center my-4">
+          <span className="brown hind-siliguri-semibold">STREET FOOD MENU</span>
+        </p>
+
+        {/* card area start */}
+
+        <div className="row">
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <PlaceCard />
+          </div>
+        </div>
+
+        {/* card area end */}
+      </div>
+    </div>
+  );
 };
 
 export default PlaceList;
