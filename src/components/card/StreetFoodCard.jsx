6a7d5c84b924b4e31e309ea8e23feefbe8d143b9
STLwin2003 <shinthantlwin70@gmail.com>
2024-06-29 09:32:25 +0630

    [update]street food card

--- src/components/card/StreetFoodCard.jsx
+++ src/components/card/StreetFoodCard.jsx
@@ -6,12 +6,12 @@ const StreetFoodCard = () => {
       <div class="">
         <img
           src="https://images.unsplash.com/photo-1551504734-5ee1c4a1479b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          class="card-img-top"
+          class="card-img-top rounded shadow-lg"
           alt="img"
         />
         <div class="card-body">
-          <p class="card-title mt-2 mb-5 brown">
-            <span className="brown fs-5">Street food</span>
+          <p class="card-title mt-2 mb-4 brown ">
+            <span className="fw-medium fs-5">Street food</span>
           </p>{" "}
         </div>
       </div>
