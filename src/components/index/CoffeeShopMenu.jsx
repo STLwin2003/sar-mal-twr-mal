81296bf6ace8d6699394f616a2051c8f9d12ef94
STLwin2003 <shinthantlwin70@gmail.com>
2024-06-29 09:30:53 +0630

    [update]coffee shop menu

--- src/components/index/CoffeeShopMenu.jsx
+++ src/components/index/CoffeeShopMenu.jsx
@@ -1,74 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import CoffeeShopCard from "../card/CoffeeShopCard";
 
 const CoffeeShopMenu = () => {
-  return <div>
-    <div className="container my-5">
-      <p class="fs-3 fw-semibold"><span className="brown">COFFEE SHOP MENU</span></p>
+  return (
+    <div>
+      <div className="container my-5">
+        <p class="fs-3 fw-semibold">
+          <span className="brown">COFFEE SHOP MENU</span>
+        </p>
 
-      <div className="row my-3">
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="">
-            <img src="https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="img" />
-            <div className="card-body">
-              <p className="card-title mt-2 mb-5 brown"><span className="brown fs-5">Coffee</span></p>  </div>
+        <div className="row my-3">
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <CoffeeShopCard />
           </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="" >
-          <img src="https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="img" />
-          <div className="card-body">
-              <p className="card-title mt-2 mb-5 brown"><span className="brown fs-5">Coffee</span></p>
-            </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <CoffeeShopCard />
           </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="" >
-          <img src="https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="img" />
-          <div className="card-body">
-              <p className="card-title mt-2 mb-5 brown"><span className="brown fs-5">Coffee</span></p>  </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <CoffeeShopCard />
           </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="" >
-          <img src="https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="img" />
-          <div className="card-body">
-              <p className="card-title mt-2 mb-5 brown"><span className="brown fs-5">Coffee</span></p>  </div>
+          <div className="col-sm-6 col-md-4 col-lg-3">
+            <CoffeeShopCard />
           </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="" >
-          <img src="https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="img" />
-          <div className="card-body">
-              <p className="card-title mt-2 mb-5 brown"><span className="brown fs-5">Coffee</span></p>  </div>
+
+          <div className="d-grid gap-2 d-md-flex justify-content-md-end ">
+            <Link
+              to="/coffee_shop_list"
+              className="btn px-4"
+              type="button"
+              style={{ background: "#b46c14", color: "white" }}
+            >
+              See More
+            </Link>
           </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="" >
-          <img src="https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="img" />
-          <div className="card-body">
-              <p className="card-title mt-2 mb-5 brown"><span className="brown fs-5">Coffee</span></p>  </div>
-          </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="" >
-          <img src="https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="img" />
-          <div className="card-body">
-              <p className="card-title mt-2 mb-5 brown"><span className="brown fs-5">Coffee</span></p>  </div>
-          </div>
-        </div>
-        <div className="col-sm-6 col-md-4 col-lg-3">
-          <div className="" >
-          <img src="https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="img" />
-          <div className="card-body">
-              <p className="card-title mt-2 mb-5 brown"><span className="brown fs-5">Coffee</span></p>  </div>
-          </div>
-        </div>
-        <div className="d-grid gap-2 d-md-flex justify-content-md-end " >
-           <button className="btn" type="button" style={{background:"#b46c14",color:"white"}}>See More</button>
         </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default CoffeeShopMenu;
