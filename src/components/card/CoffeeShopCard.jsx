ad559d99e252dba58a884eb895f8120dae68f831
STLwin2003 <shinthantlwin70@gmail.com>
2024-06-29 09:30:29 +0630

    [add]coffee shop card

--- src/components/card/CoffeeShopCard.jsx
+++ src/components/card/CoffeeShopCard.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const CoffeeShopCard = () => {
-  return <div>CoffeeShopCard</div>;
+  return (
+    <div>
+      <div className="">
+        <img
+          src="https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?auto=compress&cs=tinysrgb&w=600"
+          class="card-img-top shadow-lg rounded"
+          alt="img"
+        />
+        <div className="card-body">
+          <p className="card-title mt-2 mb-5 ">
+            <span className="fw-medium fs-5">Coffee</span>
+          </p>{" "}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CoffeeShopCard;
