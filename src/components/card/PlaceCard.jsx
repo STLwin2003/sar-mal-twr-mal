125124c13cdc5fdff2b9bf506226dc83708cc4f9
STLwin2003 <shinthantlwin70@gmail.com>
2024-06-29 09:31:41 +0630

    [add]place card

--- src/components/card/PlaceCard.jsx
+++ src/components/card/PlaceCard.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const PlaceCard = () => {
-  return <div>PlaceCard</div>;
+  return (
+    <div>
+      <div>
+        <img
+          src="https://jayscotts.com/wp-content/uploads/2022/03/restaurant-patio-design-6-821x1024.jpg"
+          className="rounded mb-3 card-img-top shadow-lg"
+          alt="Cinque Terre"
+        />
+        <p className="fs-5 mb-4 fw-medium">
+          <span className="">Pathein Park</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default PlaceCard;
