bee52225e53189cd7c8cf63a08451ea91555a39f
STLwin2003 <shinthantlwin70@gmail.com>
2024-07-05 19:27:22 +0630

    [add] item page

---
+++ src/components/admin/item/RatingItem.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import AdminFoodCard from "../card/AdminFoodCard";
+
+const RatingItem = ({ e, id }) => {
+  return (
+    <div>
+      <div className="row  rounded-4">
+        <div className="col-4 my-auto d-flex">
+          <span className="my-auto fs-6 fw-bold">{id + 1}</span>
+          <div className="p-2 text-start">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_1bpO0XDD8fbmRvnbnkCoQNFFoH3AqofVTg&s"
+              alt=""
+              className=" rounded-circle w-25 shadow-lg"
+            />
+            <span className="fs-6 ms-3 roboto-regular">{e}</span>
+          </div>
+        </div>
+        <div className="col-4 my-auto">
+          <div className="border border-dark-subtle rounded-5 p-3">
+            <i class="fa-solid fa-star fs-5 me-1"></i>
+            <i class="fa-solid fa-star fs-5 me-1"></i>
+            <i class="fa-solid fa-star fs-5 me-1"></i>
+            <i class="fa-solid fa-star fs-5 me-1"></i>
+            <i class="fa-solid fa-star fs-5 me-1"></i>
+            <i class="fa-solid fa-star fs-5 me-1"></i>
+            <i class="fa-solid fa-star fs-5 me-1"></i>
+            <i class="fa-solid fa-star fs-5 me-1"></i>
+            <i class="fa-solid fa-star fs-5 me-1"></i>
+            <i class="fa-solid fa-star fs-5 me-1"></i>
+          </div>
+        </div>
+        <div className=" col-4 my-auto">
+          <div className="w-100">
+            <AdminFoodCard />
+          </div>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default RatingItem;
