e9f22707dbff705f65f554f1a176b289cec8fa68
STLwin2003 <shinthantlwin70@gmail.com>
2024-07-05 10:59:47 +0630

    [add] addpost and event

--- src/components/admin/action/AddPost.jsx
+++ src/components/admin/action/AddPost.jsx
@@ -1,7 +1,106 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 
 const AddPost = () => {
-  return <div>AddPost</div>;
+  return (
+    <div>
+      <section className="bg-light-gray rounded-3">
+        <div className="container p-4">
+          <div className="row">
+            <h1>Add Post from admin</h1>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="mb-4">
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control border"
+                  placeholder="Enter name"
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control border"
+                  placeholder="Enter address"
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control border"
+                  placeholder="Enter Phone no:"
+                />
+              </div>
+              <div className="mb-4 ">
+                <div className="d-flex gap-3">
+                  <input
+                    type="text"
+                    name="name"
+                    className="form-control border"
+                    placeholder="Enter Opening Hour"
+                  />
+                  <input
+                    type="text"
+                    name="name"
+                    className="form-control border"
+                    placeholder="Enter Close Hour"
+                  />
+                </div>
+              </div>
+              <div className="mb-4 ">
+                <div className="d-flex gap-3">
+                  <input
+                    type="image"
+                    name="name"
+                    className="form-control border"
+                    placeholder="Enter 1st Image"
+                  />
+                  <input
+                    type="image"
+                    name="name"
+                    className="form-control border"
+                    placeholder="Enter 2nd Image"
+                  />
+                  <input
+                    type="image"
+                    name="name"
+                    className="form-control border"
+                    placeholder="Enter 3rd Image"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="mb-4">
+                <textarea
+                  name=""
+                  placeholder="Food review area"
+                  id=""
+                  rows={4}
+                  cols={50}
+                  className=" rounded p-2"
+                ></textarea>
+              </div>
+              <div className="mb-4">
+                <textarea
+                  name=""
+                  placeholder="Layout review area"
+                  id=""
+                  rows={4}
+                  cols={50}
+                  className=" rounded p-2"
+                ></textarea>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default AddPost;
--- src/components/admin/action/Event.jsx
+++ src/components/admin/action/Event.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import StreetFoodCard from "../../card/CoffeeShopCard";
 
 const Event = () => {
-  return <div>Event</div>;
+  return (
+    <div>
+      <section>
+        <div className="container-fluid">
+          <h1>Add Event from Admin</h1>
+          <div className="row my-4">
+            <div className="col-5">
+              <input
+                type="text"
+                className="form-control border-3"
+                placeholder="Enter Event Name"
+              />
+            </div>
+          </div>
+
+          <div className="row ">
+            <div className="col-3">
+              <StreetFoodCard />
+            </div>
+            <div className="col-3">
+              <StreetFoodCard />
+            </div>
+            <div className="col-3">
+              <StreetFoodCard />
+            </div>
+            <div className="col-3">
+              <StreetFoodCard />
+            </div>
+          </div>
+          <hr />
+          {/* post list here */}
+          <div>
+            <h1>Post List here</h1>
+            <div className="row">
+              <div className="col-3">
+                <StreetFoodCard />
+              </div>
+              <div className="col-3">
+                <StreetFoodCard />
+              </div>
+              <div className="col-3">
+                <StreetFoodCard />
+              </div>
+              <div className="col-3">
+                <StreetFoodCard />
+              </div>
+              <div className="col-3">
+                <StreetFoodCard />
+              </div>
+              <div className="col-3">
+                <StreetFoodCard />
+              </div>
+              <div className="col-3">
+                <StreetFoodCard />
+              </div>
+              <div className="col-3">
+                <StreetFoodCard />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Event;
