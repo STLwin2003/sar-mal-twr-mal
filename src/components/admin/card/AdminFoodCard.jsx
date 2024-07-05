e94fe11751d7ff53178ca67a0616016f5d8b7421
STLwin2003 <shinthantlwin70@gmail.com>
2024-07-05 19:27:39 +0630

    [change design] rating list

--- src/components/admin/card/AdminFoodCard.jsx
+++ src/components/admin/card/AdminFoodCard.jsx
@@ -3,39 +3,22 @@ import React from "react";
 const AdminFoodCard = () => {
   return (
     <div>
-      <div>
-        <div className="">
-          <div className="d-flex">
-            <img
-              src="https://images.unsplash.com/photo-1551504734-5ee1c4a1479b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="card-img-top rounded shadow-lg me-2"
-              alt="img"
-            />
-            <div>
-            <div className="">
-              <i class="fa-solid fa-star fs-5 text-warning "></i>
-              <span className="fs-5 fw-semibold  roboto-regular ms-2">9.4</span>
-            </div>
-            <span className=" fs-5 roboto-regular text-decoration-none">
-              Street food
-            </span>
+      <div className="row">
+        <div className="col-4">
+          <img
+            src="https://images.unsplash.com/photo-1551504734-5ee1c4a1479b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="w-100 rounded shadow-lg "
+            alt="img"
+          />
+        </div>
+        <div className="col-8 my-auto">
+          <div>
+            <i class="fa-solid fa-star fs-5 text-warning "></i>
+            <span className="fs-5 fw-semibold  roboto-regular ms-2">9.4</span>
           </div>
-          </div>
-          
-
-          {/* <div className="card-body">
-            <p className="card-title mt-2 brown">
-              <div className="">
-                <i class="fa-solid fa-star fs-5 text-warning "></i>
-                <span className="fs-5 fw-semibold  roboto-regular ms-2">
-                  9.4
-                </span>
-              </div>
-              <span className=" fs-5 roboto-regular text-decoration-none">
-                Street food
-              </span>
-            </p>
-          </div> */}
+          <span className=" fs-6 roboto-regular text-decoration-none">
+            Street Food Card
+          </span>
         </div>
       </div>
     </div>
--- src/components/admin/control/RatingList.jsx
+++ src/components/admin/control/RatingList.jsx
@@ -1,47 +1,16 @@
 import React from "react";
-import AdminFoodCard from "../card/AdminFoodCard";
+import RatingItem from "../item/RatingItem";
 
 const RatingList = () => {
+  const name = ["shin thant", "wyne htet ", "Thwe gyi"];
+
   return (
     <div>
       <section>
         <h1>Rating List for Admin</h1>
-        <div className="row border rounded-4">
-          <div className="col-3 my-auto d-flex">
-            <div className="p-4 text-center">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_1bpO0XDD8fbmRvnbnkCoQNFFoH3AqofVTg&s"
-                alt=""
-                className=" rounded-circle w-50"
-              />
-            </div>
-            <div>
-              {" "}
-              <p className="fs-3 ">Name here</p>
-              <p>sample@gmail.com</p>
-            </div>
-          </div>
-          <div className="col-4 my-auto">
-            <div className="border border-dark-subtle rounded-5 p-3">
-              <i class="fa-solid fa-star fs-4 me-2"></i>
-              <i class="fa-solid fa-star fs-4 me-2"></i>
-              <i class="fa-solid fa-star fs-4 me-2"></i>
-              <i class="fa-solid fa-star fs-4 me-2"></i>
-              <i class="fa-solid fa-star fs-4 me-2"></i>
-              <i class="fa-solid fa-star fs-4 me-2"></i>
-              <i class="fa-solid fa-star fs-4 me-2"></i>
-              <i class="fa-solid fa-star fs-4 me-2"></i>
-              <i class="fa-solid fa-star fs-4 me-2"></i>
-              <i class="fa-solid fa-star fs-4 me-2"></i>
-            </div>
-          </div>
-          <div className=" col-4 my-auto">
-            <div className=" w-50 ">
-              <AdminFoodCard />
-            </div>
-          </div>
-        </div>
-        
+        {name.map((e, id) => (
+          <RatingItem e={e} id={id} />
+        ))}
       </section>
     </div>
   );
