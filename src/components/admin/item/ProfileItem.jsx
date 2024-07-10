ab60e52b2b7b1b5ac224fe29dd1c90180f0dc963
STLwin2003 <shinthantlwin70@gmail.com>
2024-07-10 23:28:11 +0630

    [add] delete btn modal

--- src/components/admin/item/ProfileItem.jsx
+++ src/components/admin/item/ProfileItem.jsx
@@ -20,7 +20,43 @@ const ProfileItem = ({ e, id }) => {
           <a href="#!">mila@kunis.com</a>
         </td>
         <td class="text-center">
-          <i className="fa-solid fa-trash-can "></i>
+          <div className="my-auto">
+            <i
+              className="fa-solid fa-trash-can "
+              data-bs-toggle="modal"
+              data-bs-target="#delete"
+            ></i>
+
+            <div
+              class="modal fade"
+              id="delete"
+              data-bs-backdrop="static"
+              data-bs-keyboard="false"
+              tabindex="-1"
+              aria-labelledby="staticBackdropLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                  <div class="modal-body">
+                    <p className="fs-5 fw-semibold">Do you want to delete?</p>
+                  </div>
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                    >
+                      Cancel
+                    </button>
+                    <button type="button" class="btn btn-primary">
+                      Yes
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </td>
         <td className="width:20%">
           <span className="role">User</span>
