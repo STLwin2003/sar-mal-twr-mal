011ebf533e5597b63309d6f98fb04246e593144e
STLwin2003 <shinthantlwin70@gmail.com>
2024-07-12 22:25:41 +0630

    [add] map area

--- src/components/admin/card/AdminFoodCard.jsx
+++ src/components/admin/card/AdminFoodCard.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const AdminFoodCard = () => {
+const AdminFoodCard = ({ add }) => {
   return (
     <div>
       <div className="row my-2">
-        <Link to="/admin/food_detail" className="col-4">
+        <Link to="/admin/food_detail">
           <img
             src="https://images.unsplash.com/photo-1551504734-5ee1c4a1479b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            className="w-100 rounded shadow-lg "
+            className="w-100 rounded shadow-lg p-1"
             alt="img"
           />
         </Link>
-        <div className="col-8 my-auto">
+        <div>
           <div>
             <i class="fa-solid fa-star fs-5 text-warning "></i>
             <span className="fs-5 fw-semibold  roboto-regular ms-2">9.4</span>
